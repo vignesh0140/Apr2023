c360f3701c8dacaef24b493bad8eceafa467b567
vignesh0140 <vignesh0140@gmail.com>
2023-04-18 23:20:31 +0530

    task done

---
+++ src/org/test/Employee.java
@@ -0,0 +1,19 @@
+package org.test;
+
+public class Employee {
+
+	public void add() {
+
+		System.out.println("add");
+
+	}
+
+	public static void main(String[] Integer[]) {
+
+		Employee a = new Employee();
+
+		a.add();
+
+	}
+
+}
